dc6169f0a2dbce80d3b30ed00f89344d431f7cae
unknown <ragesh.sharma@cognizant.com>
2023-10-03 22:30:29 +0530

    initial commit

--- src/main/java/com/copilot/demoapi/DemoApiApplication.java
+++ src/main/java/com/copilot/demoapi/DemoApiApplication.java
@@ -7,12 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApiApplication {
 
 	public static void main(String[] args) {
-		try {
-			SpringApplication.run(DemoApiApplication.class, args);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
+		SpringApplication.run(DemoApiApplication.class, args);
 	}
 
 }
